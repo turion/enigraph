1356a5354f863023536570fba130ff1adcf3e979
Manuel Bärenz <manuel@enigmage.de>
2011-12-06 20:41:21 +0100

    Grundgerüst für neuen Node

--- __init__.py
+++ __init__.py
@@ -6,126 +6,60 @@ Provides a simple tree functionality, mainly for enigmage."""
 
 __ALL__ = [ 'directory' ]
 
-class Node(object):
-	open = False
-	def __init__(self, data=None, parent=None): # Should there be a parent keyword?
-		self.data = data
-		self.parents_history = []
-		self._childs = []
-		self._favourite_child = None
+class BaseNode(object):
+	"""You will have to implement:
+		_get_parent(self)
+		_set_parent(self, parent)
+		_add_child(self, child)
+		_remove_child(self, child)
+	"""
+	@property
+	def parent(self):
+		return self._get_parent()
+	@parent.setter
+	def parent(self, parent):
+		old_parent = self.parent
+		if old_parent != parent:
+			self._set_parent(parent)
+			try:
+				if old_parent:
+					old_parent._remove_child(self)
+			finally:
+				parent._add_child(self)
+	@property
+	def children(self):
+		return self._get_children()
+
+class Node(BaseNode):
+	def __init__(self):
+		self._children = set()
 		self._parent = None
-		self.parent = parent
-	def adopt(self, node, favourite=False):
-		"""Only does the links on the parent side. Consider the parent property if you want links on both sides. This routine is very low level because it adresses only ._childs and not .childs! Use at own risk.
-		Subclass to change the behaviour of the parent property.
-		"""
-		if node and not (node in self._childs):
-			self._childs.append(node)
-		if favourite:
-			self.favourite_child = node
-	def post_adopt(self, node):
-		"""Hook"""
-		pass
-	@property
-	def favourite_child(self):
-		return self._favourite_child
-	@favourite_child.setter
-	def favourite_child(self, child):
-		if child in self.childs or child == None:
-			self._favourite_child = child
-		else:
-			print self, ' says: "', child, '" is not my child.'
-	def disinherit(self, node, next_favourite=None):
-		"""Technical"""
-		try:
-			self._childs.remove(node)
-			node.parents_history.remove(self)
-			self.favourite_child = next_favourite # Herzlos
-		except:
-			pass
-		if node._parent == self: node._parent = None # Ganz brutal ;)
-	@property
-	def successors(self): #TODO: rewrite as generator
-		successors = []
-		next = self.successor
-		while next:
-			successors.append(next)
-			next = next.successor
-		return successors
-	@property
-	def predecessors(self):
-		predecessors = []
-		next = self.predecessor
-		while next:
-			predecessors.append(next)
-			next = next.predecessor
-		return predecessors
-	@property
-	def successor(self):
-		if self.parent:
-			childs = self.parent.childs
-			index = childs.index(self)
-			if len(childs) > index + 1:
-				return childs[index+1] # returns None if one of the ifs fails
-	@property
-	def predecessor(self):
-		if self.parent:
-			childs = self.parent.childs
-			index = childs.index(self)
-			if index > 0:
-				return childs[index-1]
-	def get_parent(self):
+	def _get_parent(self):
 		return self._parent
-	def set_parent(self, parent):
-		if parent:
-			self.parents_history.append(parent)
-			parent.adopt(self)
-			last_parent = self._parent
-			self._parent = parent
-			parent.post_adopt(self)
-			if last_parent:
-				last_parent.disinherit(self)
-		else:
-			self._parent = None
-	parent = property(get_parent, set_parent)
-	def __str__(self):
-		return str(self.data)
-	def __repr__(self):
-		return str(type(self)) + " containing " + str(self.data)
-	def elaborate_str(self, generations=-1): # TESTME
-		"""See progeny.__doc__."""
-		returnstring = str(self)
-		if generations:
-			if self.childs:
-				returnstring = '\n|-'.join( ["+" + returnstring] + ['\n  '.join(child.elaborate_str(generations=generations-1).split("\n")) for child in self.childs ] )
-		return returnstring
-	def progeny(self, generations=-1):
-		"""Recursive! Handle with care! Right now, it doesn't check any loops and duplicates! If generations is not a nonnegative integer, it will return the complete progeny."""
-		progeny = [self]
-		if generations:
-			if generations > 0: generations -= 1
-			for child in self.childs:
-				#print child
-				try:
-					progeny += child.progeny(generations)
-				except RuntimeError:
-					pass
-		return progeny
-	@property
-	def childs(self):
-		"""Read-only so far, may be with writing access in subclasses."""
-		return self._childs
+	def _set_parent(self, parent):
+		self._parent = parent
+	def _get_children(self):
+		return iter(self._children)
+	def _add_child(self, child):
+		self._children.add(child)
+	def _remove_child(self, child):
+		self._children.remove(child)
+
+class DataNode(Node):
+	def __init__(self, data):
+		self.data = data
+		Node.__init__(self)
 
 if __name__ == "__main__":
-	a = Node("Heinz")
-	ab = Node("Karl")
-	ac = Node("Dieter")
-	acd = Node("Maja")
+	a = DataNode("Opa")
+	ab = DataNode("Onkel 1")
+	ac = DataNode("Onkel 2")
+	acd = DataNode("Kind")
 	ab.parent = a
 	ac.parent = a
 	acd.parent = ac
-	print a.elaborate_str()
-	print [node.data for node in a.childs]
+	#~ print elaborate_str(a)
+	print [node.data for node in a.children]
 	acd.parent = a
-	print a.elaborate_str()
-	print [node.data for node in a.childs]
+	#~ print elaborate_str(a)
+	print [node.data for node in a.children]
